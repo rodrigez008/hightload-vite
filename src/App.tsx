c7c96704ce39a525a1034d2f604b8177318d4b9e
rodrigez008 <rodrigez008@proton.me>
2025-11-08 02:48:27 +0500

    sass-embedded

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-
-import.meta.env.VITE_API_URL;
+import s from "./styles.module.scss";
 
 function App() {
 	const [count, setCount] = useState(0);
+	console.log(s);
 
 	return (
 		<>
